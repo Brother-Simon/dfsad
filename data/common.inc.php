0883244263c34d140ec05fc243d9db8e6677e4a8
simon <mahuan1768@126.com>
2015-04-09 07:44:33 -0700

    Initial commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '192.168.0.155:3306';
+$cfg_dbname = 'decms_loc';
+$cfg_dbuser = 'simon';
+$cfg_dbpwd = 'mahuan6';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
